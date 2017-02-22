5b5eedd91f24e0388b1821452692a9f086941217
Chad Barraford <cbarraford@gmail.com>
2017-02-21 19:08:18 -0500

    some changes

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,6 @@ COPY simple.go /simple.go
 # add pipeview script
 COPY pipeview.bash /pipeview.bash
 
-EXPOSE 8080
+EXPOSE 8888
 
 ENTRYPOINT ["/pipeview.bash"]
--- pipeview.bash
+++ pipeview.bash
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-set -x
 set -e
+set -x
 
-cd / 
-git clone -o remote -b $1 https://github.com/CBarraford/pipedream-simple.git
-cd pipedream-simple
+if [ ! -d /pipedream-simple ]; then
+    cd / 
+    git clone -o remote -b $1 https://github.com/CBarraford/pipedream-simple.git
+fi
+
+cd /pipedream-simple
 go run simple.go
