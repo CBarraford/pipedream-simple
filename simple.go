7a1e24d9695bf0c1481f145186a052bfe9acd7fa
Chad Barraford <cbarraford@gmail.com>
2017-02-23 16:39:41 -0500

    tweak 5

--- simple.go
+++ simple.go
@@ -27,7 +27,7 @@ func root(w http.ResponseWriter, r *http.Request) {
 }
 
 func another(w http.ResponseWriter, r *http.Request) {
-	log.Print("newBranch4")
+	log.Print("newBranch5")
 	fmt.Fprintf(w, "newBranch")
 }
 
