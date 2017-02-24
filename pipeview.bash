a4682d86f88c6ada2380f8e703a9147b36f7077f
Chad Barraford <cbarraford@gmail.com>
2017-02-23 20:36:31 -0500

    add emoji

--- pipeview.bash
+++ pipeview.bash
@@ -1,16 +1,40 @@
 #!/bin/bash
 
+emoji=(🍔 🍪 🍕 🍖 🍗 🍘 🍙 🍚 🍛 🍜 🍝 🍞 🍟 🍣 🍤 🍥 🍱 🍲 🍳 🍴)
+selectedexpression=${expressions[$RANDOM % ${#expressions[@]} ]}
 set -e
-set -x
 
 SHA=$1
 
 if [ ! -d /pipedream-simple ]; then
     cd / 
-    git clone --progress https://github.com/CBarraford/pipedream-simple.git
+    git clone https://github.com/CBarraford/pipedream-simple.git
     cd /pipedream-simple
-    git checkout $SHA
+    git reset --hard $SHA
 fi
+
+echo 🍔; sleep 0.25
+echo 🍪; sleep 0.25
+echo 🍕; sleep 0.25
+echo 🍖; sleep 0.25
+echo 🍗; sleep 0.25
+echo 🍘; sleep 0.25
+echo 🍙; sleep 0.25
+echo 🍚; sleep 0.25
+echo 🍛; sleep 0.25
+echo 🍜; sleep 0.25
+echo 🍝; sleep 0.25
+echo 🍞; sleep 0.25
+echo 🍟; sleep 0.25
+echo 🍣; sleep 0.25
+echo 🍤; sleep 0.25
+echo 🍥; sleep 0.25
+echo 🍱; sleep 0.25
+echo 🍲; sleep 0.25
+echo 🍳; sleep 0.25
+echo 🍴; sleep 0.25
+
+set -x
 
 cd /pipedream-simple
 go run simple.go
